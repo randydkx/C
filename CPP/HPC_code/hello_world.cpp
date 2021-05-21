2339f6f06810a4af8f9abdef60a221a65f607844
randydkx <1551904915@qq.com>
2021-05-21 16:13:59 +0800

    HPC_P2

---
+++ CPP/HPC_code/hello_world.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void Hello(void);
+
+int main(int argc, char* argv[]) {
+    /* Get number of thread from command line */
+    int thread_count = 10;
+
+# pragma omp parallel num_threads(thread_count)
+    Hello();
+    return 0;
+}
+
+void Hello(void) {
+    int my_rank = omp_get_thread_num();
+    int thread_count = omp_get_num_threads();
+
+    printf("Hello from thread %d of %d\n", my_rank, thread_count);
+}
