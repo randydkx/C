cf22a1fb082c427f2d0b22227bc09a85277ad46b
randydkx <1551904915@qq.com>
2021-07-08 19:57:21 +0800

    HPC problem4

--- Python/深度学习入门与实践/test.py
+++ Python/深度学习入门与实践/test.py
@@ -1,30 +1,17 @@
+import pandas as pd
 import numpy as np
-x = np.array([3,2,1])
-index = np.argsort(x)
-print(x)
-print(index)
-print(x[index])
-print(241 * 64)
-import torch 
-x = torch.tensor([[1],[2]])
-print(x)
-print(x.size())
-print(x.squeeze().numpy())
-import nltk
-
-import ssl
-
-try:
-
-    _create_unverified_https_context = ssl._create_unverified_context
-
-except AttributeError:
-
-    pass
-
-else:
-
-    ssl._create_default_https_context = _create_unverified_https_context
-
-nltk.download_shell()
-
+import matplotlib.pyplot as plt
+x = [[-0.5,-0.5,+0.3,-0.1],[-0.5,+0.5,-0.5,+1.0]]
+# x = [[-0.5,-0.5],[-0.5,+0.5],[+0.3,-0.5],[-0.1,+1.0]]
+y = [1,1,0,0]
+plt.scatter(x[0][:2],x[1][:2],c='b')
+plt.scatter(x[0][2:],x[1][2:],c='r')
+w0 = -0.5
+w1 = -2
+b = 0
+x1 = np.linspace(-1,1.2)
+y = -w1/w0*x1-b/w1
+plt.plot(x1,y)
+plt.xlim([-0.8,0.8])
+plt.ylim([-1.2,1.2])
+plt.show()
