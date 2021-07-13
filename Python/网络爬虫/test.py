d1e015a36f199a90851042608dd43c3a4b4adcca
randydkx <1551904915@qq.com>
2021-07-13 11:55:12 +0800

    爬虫 -解析库的使用

--- Python/网络爬虫/test.py
+++ Python/网络爬虫/test.py
@@ -1,20 +1,20 @@
-from bs4 import BeautifulSoup
-soup = BeautifulSoup('<p>hello</p>','lxml')
-print(soup.p.string)
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup('<p>hello</p>','lxml')
+# print(soup.p.string)
 
 # from selenium import webdriver
 # brower = webdriver.PhantomJS()
 # brower.get("https://www.baidu.com")
 # print(brower.current_url)
 
-import pymysql
-print(pymysql.__version__)
+# import pymysql
+# print(pymysql.__version__)
 
-import tesserocr
-from PIL import Image
-image = Image.open('image.jpg')
-print(tesserocr.image_to_text(image))
+# import tesserocr
+# from PIL import Image
+# image = Image.open('image.jpg')
+# print(tesserocr.image_to_text(image))
 
-import pymongo
-print(pymongo.__version__)
+# import pymongo
+# print(pymongo.__version__)
 
