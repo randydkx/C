110bb6c31ac7bcc25ee737a13cedf03f107da5d9
randylo <1551904915@qq.com>
2022-08-28 09:30:57 +0800

    book - cmake cookbook

---
+++ CPP/CMake/cmake-cookbook/chapter-01/recipe-09/cxx-example/build/CMakeFiles/animal-farm.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for animal-farm.
+# This may be replaced when dependencies are built.
---
+++ CPP/CMake/cmake-cookbook/chapter-01/recipe-10/cxx-example/build/CMakeFiles/geometry.dir/geometry_rhombus.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/geometry.dir/geometry_rhombus.cpp.o: \
+  /Users/wenshuiluo/coding/CPP/CMakeTest/cmake-cookbook/chapter-01/recipe-10/cxx-example/geometry_rhombus.cpp \
+  /Users/wenshuiluo/coding/CPP/CMakeTest/cmake-cookbook/chapter-01/recipe-10/cxx-example/geometry_rhombus.hpp
